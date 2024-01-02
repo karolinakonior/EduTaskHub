7c04ac7f94efe5d21fa63669fdf6dfca5d9c2e3d
karolinakonior <karolina.koniorr@gmail.com>
2024-01-02 09:27:33 +0000

    Completed endpoints API

--- src/MVC/routers/api-router.ts
+++ src/MVC/routers/api-router.ts
@@ -3,8 +3,10 @@ import { studentsRouter } from "./students-router";
 import { subjectsRouter } from "./subjects-router";
 import { yearsRouter } from "./years-router";
 import { assignmentsRouter } from "./assignments-router";
+const { getAllEndpoints } = require("../controllers/EndPointController");
 export const apiRouter = require("express").Router();
 
+apiRouter.get("/", getAllEndpoints);
 apiRouter.use("/teachers", teachersRouter);
 apiRouter.use("/students", studentsRouter);
 apiRouter.use("/subjects", subjectsRouter);
